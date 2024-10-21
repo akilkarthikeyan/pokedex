5110d08e24b0bd445b7d68e2c9eab345420d1093
Akil Karthikeyan <akilkarthikeyan@outlook.com>
2024-10-21 11:24:07 -0500

    Fix unused vars error

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import DetailView from './pages/DetailView';
